b184148f3d7b67b508e1ef79931bccfbb31e26ee
edwinc456 <skydude51@yahoo.com>
2014-03-09 16:44:08 -0700

    Week 4 homework

---
+++ Week3Lab4/src/Lab4.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Lab4 {
+
+	public static void main(String[] args)
+	
+	{
+
+		Scanner input = new Scanner(System.in);
+	
+		int number1;
+		int number2;
+		
+		System.out.println("Enter first interger: ");
+			number1=input.nextInt();
+		System.out.println("Enter second interger: ");
+			number2=input.nextInt();
+		System.out.printf("Your numbers are %d and %d.", number1 , number2 );
+	}
+		
+}
